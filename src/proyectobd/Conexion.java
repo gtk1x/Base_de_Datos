73430369e4f0452eeafb4b3f6adb50d78390d945
Gerardod32 <gerardod32@gmail.com>
2016-05-12 00:02:11 -0600

    Primera

---
+++ src/proyectobd/Conexion.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectobd;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Conexion {
+Connection conect = null;
+   public Connection conexion()
+    {
+      try {
+             
+           //Cargamos el Driver MySQL
+           Class.forName("com.mysql.jdbc.Driver");
+           conect = DriverManager.getConnection("jdbc:mysql://localhost/mydb","BIMAR","A0E9M0L3N");
+           //JOptionPane.showMessageDialog(null, "conectado");
+           //Cargamos el Driver Access
+           //Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+           //Conectar en red base 
+           //String strConect = "jdbc:odbc:Driver=Microsoft Access Driver (*.mdb);DBQ=//servidor/bd_cw/cw.mdb";
+           //Conectar Localmente
+           //String strConect = "jdbc:odbc:Driver=Microsoft Access Driver (*.mdb);DBQ=D:/cwnetbeans/cw.mdb";
+          //conect = DriverManager.getConnection(strConect,"",""); 
+          } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error "+e);
+        }
+        return conect;
+     }}
